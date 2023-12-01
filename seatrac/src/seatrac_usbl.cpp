f9d6c2c60e134587f5bef74006b012e6096205c6
jasonmiller1819 <jason_miller@uri.edu>
2023-12-01 13:55:11 -0500

    init service calls for proto commands

--- seatrac/src/seatrac_usbl.cpp
+++ seatrac/src/seatrac_usbl.cpp
@@ -23,58 +23,184 @@
 
 #include "seatrac_usbl.hpp"
 
+
+/**
+ * @brief Construct a new Sea Trac USBL:: SeaTrac USBL object
+ * 
+ */
 SeaTracUSBL::SeaTracUSBL()
 {
     m_nh.reset(new ros::NodeHandle(""));
     m_pnh.reset(new ros::NodeHandle("~"));
 
-    health_publisher = m_nh->advertise<mvp_msgs::ModemHealth>("usbl/health", 10);
-    pose_publisher = m_nh->advertise<mvp_msgs::ModemPose>("usbl/pose", 10);
+    odom_pub = m_nh->advertise<nav_msgs::Odometry>("comms/odometry/filtered/local", 10);
+    health_pub = m_nh->advertise<mvp_msgs::VehicleStatus>("comms/health", 10);
+
+    pose_server = m_nh->advertiseService
+        <mvp_msgs::CommsPose::Request, 
+        mvp_msgs::CommsPose::Response>
+    (
+        "comms/request_pose",
+        std::bind(
+            &SeaTracUSBL::f_cb_srv_request_pose,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2
+        )
+    );
+
+    health_server = m_nh->advertiseService
+        <mvp_msgs::CommsHealth::Request,
+        mvp_msgs::CommsHealth::Response>
+    (
+        "comms/request_health",
+        std::bind(
+            &SeaTracUSBL::f_cb_srv_request_health,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2
+        )    
+    );
+
+    relative_pose_server = m_nh->advertiseService
+        <mvp_msgs::CommsRelativePose::Request,
+        mvp_msgs::CommsRelativePose::Response>
+    (
+        "comms/request_relative_pose",
+        std::bind(
+            &SeaTracUSBL::f_cb_srv_request_relative_pose,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2
+        )
+    );
+
+    controller_info_server = m_nh->advertiseService
+        <mvp_msgs::CommsControllerInfo::Request,
+        mvp_msgs::CommsControllerInfo::Response>
+    (
+        "comms/reuest_controller_info",
+        std::bind(
+            &SeaTracUSBL::f_cb_srv_controller_info,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2
+        )
+    );
+
+    direct_control_server = m_nh->advertiseService
+        <mvp_msgs::CommsDirectControl::Request,
+        mvp_msgs::CommsDirectControl::Response>
+    (
+        "comms/direct_control",
+        std::bind(
+            &SeaTracUSBL::f_cb_srv_direct_control,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2
+        )
+    );
+
+    state_info_server = m_nh->advertiseService
+        <mvp_msgs::CommsStateInfo::Request,
+        mvp_msgs::CommsStateInfo::Response>
+    (
+        "comms/state_info",
+        std::bind(
+            &SeaTracUSBL::f_cb_srv_state_info,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2
+        )
+    );
+
+    single_waypoint_server = m_nh->advertiseService
+        <mvp_msgs::CommsSingleWaypoint::Request,
+        mvp_msgs::CommsSingleWaypoint::Response>
+    (
+        "comms/single_waypoint",
+        std::bind(
+            &SeaTracUSBL::f_cb_srv_single_waypoint,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2
+        )
+    );
+
+    multi_waypoint_gps_server = m_nh->advertiseService
+        <mvp_msgs::CommsMultiWaypointGPS::Request,
+        mvp_msgs::CommsMultiWaypointGPS::Response>
+    (
+        "comms/multi_waypoint_gps",
+        std::bind(
+            &SeaTracUSBL::f_cb_srv_multi_waypoint_gps,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2
+        )
+    );
+
+    multi_waypoint_xyz_server = m_nh->advertiseService
+        <mvp_msgs::CommsMultiWaypointXYZ::Request,
+        mvp_msgs::CommsMultiWaypointXYZ::Response>
+    (
+        "comms/multi_waypoint_xyz",
+        std::bind(
+            &SeaTracUSBL::f_cb_srv_multi_waypoint_xyz,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2
+        )
+    );
+
+    execute_waypoints_server = m_nh->advertiseService
+        <mvp_msgs::CommsExecuteWaypoint::Request,
+        mvp_msgs::CommsExecuteWaypoint::Response>
+    (
+        "comms/execute_waypoint",
+        std::bind(
+            &SeaTracUSBL::f_cb_srv_execute_waypoints,
+            this,
+            std::placeholders::_1,
+            std::placeholders::_2
+        )
+    );
 
     setup_goby();
-    
-    ros::Rate rate(10);
 
-    // Pose p_msg;
-    // p_msg.set_cmd_resp(true);
-    // q_manager.push_message(p_msg);
-
-    Health h_msg;
-    h_msg.set_destination(1);
-    h_msg.set_cmd_resp(true);
-    q_manager.push_message(h_msg);
-    int i = 0;
-
+    //loop at 10Hz 
     while(ros::ok())
     {
-        i++;
         st_driver.do_work();
         q_manager.do_work();
         mac.do_work();
-        
-                // // send another transmission every 10 seconds
-        if (!(i % 200))
-            i = 0;
-            // q_manager.push_message(p_msg);
-            q_manager.push_message(h_msg);
 
         usleep(100000);
     }
+    
 }
 
+/**
+ * @brief Destroy the SeaTracUSBL:: SeaTracUSBL object
+ * 
+ */
 SeaTracUSBL::~SeaTracUSBL()
 {
 
 }
 
+/**
+ * @brief the goby dccl, mac, queue, and driver are configured and initialized
+ * 
+ */
 void SeaTracUSBL::setup_goby()
 {
     goby::acomms::bind(st_driver, q_manager, mac);
 
     // set the source id of this modem
-    uint32_t our_id = 15;
-    uint32_t dest_id = 1;
-    uint32_t slot_time = 5;
+    our_id = 15;
+    dest_id = 1;
+    slot_time = 5;
 
     //Initiate DCCL
     goby::acomms::protobuf::DCCLConfig dccl_cfg;
@@ -87,11 +213,13 @@ void SeaTracUSBL::setup_goby()
     dccl_->validate<DirectControl>();
     dccl_->validate<StateInfo>();
     dccl_->validate<SingleWaypoint>();
-    dccl_->validate<MultiWaypoint>();
+    dccl_->validate<MultiWaypointGPS>();
+    dccl_->validate<MultiWaypointXYZ>();
     dccl_->validate<ExecuteWaypoints>();
 
-    //setup protobuf messages
-    setup_msgs();
+
+    //setup the goby queue manager
+    setup_queue();
     
     q_manager_cfg.set_modem_id(our_id);
 
@@ -100,7 +228,7 @@ void SeaTracUSBL::setup_goby()
     //Initiate modem driver
     goby::acomms::protobuf::DriverConfig driver_cfg;
     driver_cfg.set_modem_id(our_id);
-    driver_cfg.set_serial_port("/dev/ttyUSB1");
+    driver_cfg.set_serial_port("/dev/ttyUSB0");
     driver_cfg.set_connection_type(goby::acomms::protobuf::DriverConfig_ConnectionType_CONNECTION_SERIAL);
 
     //Initiate medium access control
@@ -135,7 +263,7 @@ void SeaTracUSBL::setup_goby()
 
 
     goby::glog.set_name("usbl");
-    goby::glog.add_stream(goby::util::logger::QUIET, &std::clog);
+    goby::glog.add_stream(goby::util::logger::DEBUG2, &std::clog);
 
     dccl_->set_cfg(dccl_cfg);
     q_manager.set_cfg(q_manager_cfg);   
@@ -143,7 +271,11 @@ void SeaTracUSBL::setup_goby()
     st_driver.startup(driver_cfg);
 }
 
-void SeaTracUSBL::setup_msgs()
+/**
+ * @brief messages from mvp_messages documentation are loaded and configured into the queue manager.
+ * 
+ */
+void SeaTracUSBL::setup_queue()
 {
     //setup pose msg
     goby::acomms::protobuf::QueuedMessageEntry* q_entry_pose = q_manager_cfg.add_message_entry();
@@ -164,31 +296,40 @@ void SeaTracUSBL::setup_msgs()
     q_entry_rel_pose->set_max_queue(1);
 
     //setup controller info
-    goby::acomms::protobuf::QueuedMessageEntry* q_entry_controll = q_manager_cfg.add_message_entry();
-    q_entry_controll->set_protobuf_name("ControllerInfo");;
-    q_entry_controll->set_ack(false);
-    q_entry_controll->set_max_queue(1);
+    goby::acomms::protobuf::QueuedMessageEntry* q_entry_control = q_manager_cfg.add_message_entry();
+    q_entry_control->set_protobuf_name("ControllerInfo");;
+    q_entry_control->set_ack(false);
+    q_entry_control->set_max_queue(1);
 
+    //setup direct control
     goby::acomms::protobuf::QueuedMessageEntry* q_entry_direct = q_manager_cfg.add_message_entry();
     q_entry_direct->set_protobuf_name("DirectControl");;
     q_entry_direct->set_ack(false);
     q_entry_direct->set_max_queue(1);
 
+    //setup state info
     goby::acomms::protobuf::QueuedMessageEntry* q_entry_state = q_manager_cfg.add_message_entry();
     q_entry_state->set_protobuf_name("StateInfo");;
     q_entry_state->set_ack(false);
     q_entry_state->set_max_queue(1);
 
+    //setup single waypoint
     goby::acomms::protobuf::QueuedMessageEntry* q_entry_single_wpt = q_manager_cfg.add_message_entry();
     q_entry_single_wpt->set_protobuf_name("SingleWaypoint");;
     q_entry_single_wpt->set_ack(false);
     q_entry_single_wpt->set_max_queue(1);
 
-    goby::acomms::protobuf::QueuedMessageEntry* q_entry_multi_wpt = q_manager_cfg.add_message_entry();
-    q_entry_multi_wpt->set_protobuf_name("MultiWaypoint");;
-    q_entry_multi_wpt->set_ack(false);
-    q_entry_multi_wpt->set_max_queue(1);
+    goby::acomms::protobuf::QueuedMessageEntry* q_entry_multi_wpt_gps = q_manager_cfg.add_message_entry();
+    q_entry_multi_wpt_gps->set_protobuf_name("MultiWaypointGPS");;
+    q_entry_multi_wpt_gps->set_ack(false);
+    q_entry_multi_wpt_gps->set_max_queue(1);
 
+    goby::acomms::protobuf::QueuedMessageEntry* q_entry_multi_wpt_xyz = q_manager_cfg.add_message_entry();
+    q_entry_multi_wpt_xyz->set_protobuf_name("MultiWaypointXYZ");;
+    q_entry_multi_wpt_xyz->set_ack(false);
+    q_entry_multi_wpt_xyz->set_max_queue(1);
+
+    //setup multi waypoint
     goby::acomms::protobuf::QueuedMessageEntry* q_entry_exe_wpt = q_manager_cfg.add_message_entry();
     q_entry_exe_wpt->set_protobuf_name("ExecuteWaypoints");;
     q_entry_exe_wpt->set_ack(false);
@@ -197,21 +338,134 @@ void SeaTracUSBL::setup_msgs()
 
 }
 
-void SeaTracUSBL::received_data(const google::protobuf::Message& message_in)
+/**
+ * @brief the slot that is called back from seatrac_driver when a new message is received. the incoming 
+ * message is parsed acording to the mvp_messages documentation.
+ * 
+ * @param data_msg the incoming protobuf message
+ */
+void SeaTracUSBL::received_data(const google::protobuf::Message& data_msg)
 {
-    std::cout << "Received Data: " << message_in << std::endl;
+    std::string msg_type =  data_msg.GetTypeName();
+    if(msg_type == "Pose")
+    {
+        Pose pose;
+        pose.CopyFrom(data_msg);
+
+        //if the received message is a pose command/request
+        if(!pose.cmd_resp())
+        {
+            nav_msgs::Odometry odom;
+
+            odom.header.stamp = ros::Time::now();
+
+            odom.header.frame_id = "alpha_rise/odom";
+
+            odom.pose.pose.position.x = pose.local_x();
+            odom.pose.pose.position.y = pose.local_y();
+            odom.pose.pose.position.z = pose.local_z();
+            
+            odom.pose.pose.orientation.x = pose.x_rot();
+            odom.pose.pose.orientation.y = pose.y_rot();
+            odom.pose.pose.orientation.z = pose.z_rot();
+            odom.pose.pose.orientation.w = pose.w_rot();
+
+
+            odom_pub.publish(odom);
+
+        }
+    }
+    else if(msg_type == "Health")
+    {
+        Health health;
+        health.CopyFrom(data_msg);
+        if(!health.cmd_resp())
+        {
+            mvp_msgs::VehicleStatus status;
+
+            status.current = health.current();
+            status.voltage = health.batt_volt();
+
+            health_pub.publish(status);
+        }
+    }  
 }
 
+/**
+ * @brief TODO: Enable the ack protocol to confirm messages are being routed correctly
+ * 
+ * @param ack_message 
+ * @param original_message 
+ */
 void SeaTracUSBL::received_ack(const goby::acomms::protobuf::ModemTransmission& ack_message,
                   const google::protobuf::Message& original_message)
 {
-    std::cout << ack_message.src() << " acknowledged receiving message: " << original_message << std::endl;
+    
+    std::cout << ack_message.src() << " acknowledged receiving message: " << original_message.ShortDebugString() << std::endl;
+}
+
+bool SeaTracUSBL::f_cb_srv_request_pose(mvp_msgs::CommsPose::Request &request, mvp_msgs::CommsPose::Response &response)
+{
+
+    return true;
+}
+
+bool SeaTracUSBL::f_cb_srv_request_health(mvp_msgs::CommsHealth::Request &request, mvp_msgs::CommsHealth::Response &response)
+{
+    
+    return true;
+}
+
+bool SeaTracUSBL::f_cb_srv_request_relative_pose(mvp_msgs::CommsRelativePose::Request &request, mvp_msgs::CommsRelativePose::Response &response)
+{
+
+    return true;
+}
+
+bool SeaTracUSBL::f_cb_srv_controller_info(mvp_msgs::CommsControllerInfo::Request &request, mvp_msgs::CommsControllerInfo::Response &response)
+{
+
+    return true;
+}
+
+bool SeaTracUSBL::f_cb_srv_direct_control(mvp_msgs::CommsDirectControl::Request &request, mvp_msgs::CommsDirectControl::Response &response)
+{
+
+    return true;
+}
+
+bool SeaTracUSBL::f_cb_srv_state_info(mvp_msgs::CommsStateInfo::Request &request, mvp_msgs::CommsStateInfo::Response &response)
+{
+
+    return true;
+}
+
+bool SeaTracUSBL::f_cb_srv_single_waypoint(mvp_msgs::CommsSingleWaypoint::Request &request, mvp_msgs::CommsSingleWaypoint::Response &response)
+{
+
+    return true;
+}
+
+bool SeaTracUSBL::f_cb_srv_multi_waypoint_gps(mvp_msgs::CommsMultiWaypointGPS::Request &request, mvp_msgs::CommsMultiWaypointGPS::Response &response)
+{
+
+    return true;
+}
+bool SeaTracUSBL::f_cb_srv_multi_waypoint_xyz(mvp_msgs::CommsMultiWaypointXYZ::Request &request, mvp_msgs::CommsMultiWaypointXYZ::Response &response)
+{
+
+    return true;
+}
+bool SeaTracUSBL::f_cb_srv_execute_waypoints(mvp_msgs::CommsExecuteWaypoint::Request &request, mvp_msgs::CommsExecuteWaypoint::Response &response)
+{
+
+    return true;
 }
 
 int main(int argc, char* argv[])
 {
 
-    ros::init(argc, argv, "seatrac");
+    ros::init(argc, argv, "seatrac_usbl");
 
     SeaTracUSBL d;
 
--- seatrac/src/seatrac_usbl.hpp
+++ seatrac/src/seatrac_usbl.hpp
@@ -24,30 +24,36 @@
 
 #include <thread>
 #include <stdlib.h>
-#include <std_msgs/Header.h>
-#include <mvp_msgs/ModemHealth.h>
-#include <mvp_msgs/ModemPose.h>
-#include <std_msgs/Header.h>
 #include "ros/ros.h"
 #include "unistd.h"
 #include <iostream>
 
+#include <nav_msgs/Odometry.h>
+#include <mvp_msgs/VehicleStatus.h>
+#include <mvp_msgs/CommsPose.h>
+#include <mvp_msgs/CommsHealth.h>
+#include <mvp_msgs/CommsRelativePose.h>
+#include <mvp_msgs/CommsControllerInfo.h>
+#include <mvp_msgs/CommsDirectControl.h>
+#include <mvp_msgs/CommsStateInfo.h>
+#include <mvp_msgs/CommsSingleWaypoint.h>
+#include <mvp_msgs/CommsMultiWaypointGPS.h>
+#include <mvp_msgs/CommsMultiWaypointXYZ.h>
+#include <mvp_msgs/CommsExecuteWaypoint.h>
+
+//goby includes
 #include <goby/acomms/connect.h>
-#include <goby/acomms/bind.h>
 #include <goby/acomms/queue.h>
-#include <goby/acomms/amac.h>
-#include <goby/acomms/dccl.h>
+#include <goby/acomms/bind.h>
 #include <goby/acomms/modem_driver.h>
 #include <goby/util/binary.h>
 #include <goby/util/debug_logger.h>
+
+//protobuf msg includes
 #include "proto/goby_msgs.pb.h"
+
+//driver includes
 #include "seatrac_driver.h"
-
-#include "io.h"
-
-#include <vector>
-
-class CSeatrac;
 
 class SeaTracUSBL{
 
@@ -55,13 +61,25 @@ private:
     ros::NodeHandlePtr m_nh;
     ros::NodeHandlePtr m_pnh;
     
-    ros::Publisher health_publisher;
-    ros::Publisher pose_publisher;
+    ros::Publisher odom_pub;
+    ros::Publisher health_pub;
+    
+    ros::ServiceServer pose_server;
+    ros::ServiceServer health_server;
+    ros::ServiceServer relative_pose_server;
+    ros::ServiceServer controller_info_server;
+    ros::ServiceServer direct_control_server;
+    ros::ServiceServer state_info_server;
+    ros::ServiceServer single_waypoint_server;
+    ros::ServiceServer multi_waypoint_gps_server;
+    ros::ServiceServer multi_waypoint_xyz_server;
+    ros::ServiceServer execute_waypoints_server;
 
     bool console_debug;
     int beacon_id;
-
-    std::string buffer;
+    uint32_t our_id;
+    uint32_t dest_id;
+    uint32_t slot_time;
 
     goby::acomms::DCCLCodec* dccl_ = goby::acomms::DCCLCodec::get();
     goby::acomms::QueueManager q_manager;
@@ -71,9 +89,26 @@ private:
     goby::acomms::protobuf::QueueManagerConfig q_manager_cfg;
 
     void setup_goby();
-    void setup_msgs();
+    void setup_queue();
     void received_data(const google::protobuf::Message& message_in);
     void received_ack(const goby::acomms::protobuf::ModemTransmission& ack_message, const google::protobuf::Message& original_message);
+    bool f_cb_srv_request_pose(mvp_msgs::CommsPose::Request &request, mvp_msgs::CommsPose::Response &response);
+    bool f_cb_srv_request_health(mvp_msgs::CommsHealth::Request &request, mvp_msgs::CommsHealth::Response &response);
+    bool f_cb_srv_request_relative_pose(mvp_msgs::CommsRelativePose::Request &request, mvp_msgs::CommsRelativePose::Response &response);
+    bool f_cb_srv_controller_info(mvp_msgs::CommsControllerInfo::Request &request, mvp_msgs::CommsControllerInfo::Response &response);
+    bool f_cb_srv_direct_control(mvp_msgs::CommsDirectControl::Request &request, mvp_msgs::CommsDirectControl::Response &response);
+    bool f_cb_srv_state_info(mvp_msgs::CommsStateInfo::Request &request, mvp_msgs::CommsStateInfo::Response &response);
+    bool f_cb_srv_single_waypoint(mvp_msgs::CommsSingleWaypoint::Request &request, mvp_msgs::CommsSingleWaypoint::Response &response);
+    bool f_cb_srv_multi_waypoint_gps(mvp_msgs::CommsMultiWaypointGPS::Request &request, mvp_msgs::CommsMultiWaypointGPS::Response &response);
+    bool f_cb_srv_multi_waypoint_xyz(mvp_msgs::CommsMultiWaypointXYZ::Request &request, mvp_msgs::CommsMultiWaypointXYZ::Response &response);
+    bool f_cb_srv_execute_waypoints(mvp_msgs::CommsExecuteWaypoint::Request &request, mvp_msgs::CommsExecuteWaypoint::Response &response);
+
+
+
+
+
+
+
     ros::Timer timer;
     
 
